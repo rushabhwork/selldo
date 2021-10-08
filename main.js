5682b9a5756da91cde381aa3164b6ce29c1384a6
unknown <rushabhwork8@gmail.com>
2021-10-08 15:58:25 +0530

    first commit index file

---
+++ main.js
@@ -0,0 +1,62 @@
+
+// -------responcive Button-------
+
+$(document).ready(function(){
+    $(".responcive_button").click(function(){
+      $(".responcive_menu").toggle(300);
+    });
+  });
+
+
+
+  $('.gall').slick();
+
+  // -----------slider with js----------
+
+  
+// let arr=document.getElementsByClassName("g")
+// let num=document.getElementById("button_text")
+//  let arrnew = [...arr]  
+
+// let count=0
+
+
+
+// function counthigh(n){
+//     count=count+1
+//     if(count>4 ){count=1}
+//     console.log(count)
+//     for(let i = 0; i < arrnew.length; i++){
+//         arrnew[i].style.display="none"
+//     }
+//     var j=arrnew[count].style.display="block"
+    
+//     num.innerText=count+"/4"
+//     console.log(num)
+// }
+
+
+// function countlow(n){
+//   count=count-1
+//     if(count<1){count=4}
+//     console.log(count)
+
+//     for(let i = 0; i < arrnew.length; i++){
+//         arrnew[i].style.display="none"
+//     }
+//     var j=arrnew[count].style.display="block"
+
+//        num.innerText=count+"/4"
+// }
+
+
+// (()=>{
+//     for(let i = 0; i < arrnew.length; i++){
+//         arrnew[i].style.display="none"
+//     }
+// })()
+
+
+//   var j=arrnew[0].style.display="block"
+//   num.innerText=count+"/4"
+
